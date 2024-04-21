53de972d73067bb1149fb185df79d7dbd754d047
vitalik kovtiukh <106430549+Vitalikkov@users.noreply.github.com>
2024-04-21 13:20:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import AboutComponent from '@/components/about/about.component';
+import AboutComponent from './components/about/about.component';
 
 import { HeaderComponent } from './components/header/Header.component';
 
